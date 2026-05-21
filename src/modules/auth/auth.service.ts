d607580aabe6514585d8b1b9ced3480b6d84662d
sohag142 <sohag2879@gmail.com>
2026-05-21 08:51:37 +0600

    singup succesfully in databse

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,34 @@
+import { pool } from "../../db";
+import type { IUser } from "./auth.interface";
+import bcrypt from "bcryptjs";
 
 
-const createUserIntoDB = async() => {
+const createUserIntoDB = async (payload: IUser) => {
+    const { name, email, password, role } = payload;
 
+
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await pool.query(`
+        SELECT * FROM users WHERE email = $1
+    `, [email]);
+
+    if (existingUser.rows.length > 0) {
+        throw new Error("User with this email already exists");
+    }
+
+    const result = await pool.query(`
+        INSERT INTO users (name, email, password, role)
+        VALUES ($1, $2, $3, COALESCE($4, 'contributor'))
+        RETURNING *
+    `, [name, email, hashedPassword, role]);
+
+    delete result.rows[0].password;
+
+    return result;
 }
 
 export const authService = {
-  createUserIntoDB
+    createUserIntoDB
 }
