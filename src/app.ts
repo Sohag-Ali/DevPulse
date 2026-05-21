94089e51de6ec3c11fbcc926553ac270ad9c67a2
sohag142 <sohag2879@gmail.com>
2026-05-21 16:00:40 +0600

    Global Error Handle

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,30 @@
 
 import express, { type Application } from 'express'
-
-
 import { authRouter } from './modules/auth/auth.route'
-
 import logger from './middleware/logger';
 import { issueRouter } from './modules/issue/issue.route';
+import cors from 'cors';
+import config from './config';
+import globalErrorHandler from './middleware/globalErrorHandler';
 
 const app: Application = express()
 
 app.use(express.json())
-
 app.use(logger);
+
+const corsOptions = {
+    origin: config.clientUrl,
+    
+};
+
+app.use(cors(corsOptions));
 
 
 app.use('/api/auth', authRouter);
-
 app.use('/api/issues', issueRouter);
+
+app.use(globalErrorHandler);
+ 
+
 
 export default app;
--- src/config/index.ts
+++ src/config/index.ts
@@ -10,6 +10,7 @@ const config = {
   port: process.env.PORT || 5000,
   connectionString: process.env.CONNECTION_STRING as string || '',
   jwtSecret: process.env.JWT_SECRET as string,
+  clientUrl: process.env.CLIENT_URL as string || 'http://localhost:5000',
 };
 
 export default config;
